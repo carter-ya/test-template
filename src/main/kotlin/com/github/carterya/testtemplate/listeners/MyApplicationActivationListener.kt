8e812649df2e0d9081fe84ce7a2d963daa15585d
GitHub Action <action@github.com>
2023-11-23 10:38:18 +0000

    Template cleanup

--- src/main/kotlin/com/github/carterya/testtemplate/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/carterya/testtemplate/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.carterya.testtemplate.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
